be97f2f9741e3edb35bc83f606f31a63140dbb76
gustavobaltazar <gustavobaltazar31@gmail.com>
2022-10-26 23:09:54 -0300

    📦 NEW:  Task Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header"
 import { Post } from "./components/Post"
+import { Task } from "./components/Task"
 
 export function App() {
   return (
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,20 +1,23 @@
 import { PlusCircle } from "phosphor-react";
+import { Task } from "./Task";
 
 export function Post() {
-    return(
-        <article className="w-full flex justify-center gap-2">
-            <form className="w-3/5 flex flex-row gap-2">
-                <textarea
-                    className="w-full border-0 p-1 rounded-lg" 
+    return (
+        <article className="w-full flex flex-col items-center justify-center gap-2 p-0 absolute">
+            <form className="w-2/6 flex flex-row gap-2">
+                <input
+                    className="w-full p-1 rounded h-14 box-border grow-0 bg-gray5 resize-none text-gray3 border border-solid shadow-current border-gray7"
                     name="todo"
                     placeholder="Adicione uma nova tarefa"
                     required
                 />
-                <button className="bg-blueDark rounded-lg p-4">
+                <button className="bg-blueDark rounded-lg p-4 h-14 w-24 flex flex-row gap-1 justify-center items-center text-white">
                     Criar
-                    <PlusCircle className="text-white" size={32}/>
+                    <PlusCircle className="text-white w-4 h-4" size={32} />
                 </button>
             </form>
+
+            <Task />
         </article>
     )
 }
